41bfca068e8c1e3a52157ba6dbeff9c58fec28e6
fh0592x <fh0592x@gmail.com>
2019-11-28 01:08:21 -0800

    wrote verifyToken() utility.

--- api/utils.js
+++ api/utils.js
@@ -1,6 +1,22 @@
+import User from './models/user';
+import jwt from 'jsonwebtoken';
+
 const utils = {
     handleError(res, status, message) {
         return res.status(status).json({ payload: null, message });
+    },
+    verifyToken(req, res, next) {
+        const authHeader = req.headers['authorization'];
+        if (!!authHeader) {
+            const id = jwt.decode(authHeader, { json: true, key: process.env.SECRET }).id;
+            User.findById(id)
+            .then(user => {
+                if (!user) return utils.handleError(res, 404, `User does not exist.`);
+                return next();
+            })
+            .catch(err => utils.handleError(res, 500, err.message || err));
+        }
+        return utils.handleError(res, 400, `Invalid authorization header.`);
     }
 };
 
