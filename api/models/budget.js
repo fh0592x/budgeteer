d081e0a9d238305443be601d693188af723ede27
fh0592x <fh0592x@gmail.com>
2019-11-27 23:18:08 -0800

    wrote BUDGET model.

---
+++ api/models/budget.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from 'mongoose';
+
+const BudgetSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        index: {
+            unique: true
+        }
+    },
+    amount: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    totalExpended: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    expenses: [
+        new Schema({
+            name: {
+                type: String,
+                required: true
+            },
+            amount: {
+                type: Number,
+                required: true,
+                min: 0
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now()
+            }
+        })
+    ]
+});
+
+BudgetSchema.pre('save', function(next) {
+    const budget = this;
+    budget.totalExpended = budget.expenses.reduce((acc, val) => acc + val.amount, 0);
+    return next();
+});
+
+export default mongoose.model('User', UserSchema);
