f053c8add8d583fa1720371c5b27f6e524e3224f
fh0592x <fh0592x@gmail.com>
2019-11-28 09:30:54 -0800

    wrote Alert component.

---
+++ client/src/components/Alert.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="alert bg-danger text-white fade show" role="alert">
+      <i class="fas fa-exclamation-circle mr-2"></i>{{ error }}
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['error']
+};
+</script>
+
+<style>
+</style>
