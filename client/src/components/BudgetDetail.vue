db4623e21e5e4e9bfe2fd8671f0a02553ad4029c
fh0592x <fh0592x@gmail.com>
2019-11-29 08:53:29 -0800

    wrote BudgetDetail component.

---
+++ client/src/components/BudgetDetail.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card p-4">
+    <div class="card-body">
+      <div class="d-flex">
+        <div class="mr-auto">
+          <small class="text-muted">Budget Name</small>
+          <h2>{{ budget.name }}</h2>
+        </div>
+        <div class="mr-4">
+          <small class="text-muted">Total Amount</small>
+          <h2>{{ budget.amount }} &bigcup;</h2>
+        </div>
+        <div>
+          <small class="text-muted">Total Expended</small>
+          <h2>{{ budget.totalExpended }} &bigcup;</h2>
+        </div>
+      </div>
+      <div class="d-flex flex-column mt-2">
+        <small class="text-muted mb-2">Budget Incomes</small>
+        <div class="list-group">
+          <li class="list-group-item" v-for="inc of budget.incomes" :key="inc._id">
+            <div class="d-flex">
+              <div class="mr-auto">{{ inc.name }}</div>
+              <div>{{ inc.amount }} &bigcup;</div>
+            </div>
+          </li>
+        </div>
+      </div>
+      <div class="d-flex flex-column mt-4">
+        <small class="text-muted mb-2">Budget Expenses</small>
+        <div class="list-group">
+          <li class="list-group-item" v-for="exp of budget.expenses" :key="exp._id">
+            <div class="d-flex">
+              <div class="mr-auto">{{ exp.name }}</div>
+              <div>{{ exp.amount }} &bigcup;</div>
+            </div>
+          </li>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['budget']
+};
+</script>
+
+<style>
+</style>
