38bbc81c4c63cfc74a8ad0bd43c220787c82573f
fh0592x <fh0592x@gmail.com>
2019-11-29 13:26:33 -0800

    wrote handler for budget deletion.

--- client/src/views/Budget.vue
+++ client/src/views/Budget.vue
@@ -3,9 +3,23 @@
     <div class="row">
       <div class="col-3">
         <ul class="list-group list-group-flush mb-2">
-          <router-link exact tag="a" class="list-group-item list-group-item-action" to="/app/new-budget">New Budget</router-link>
-          <a class="list-group-item list-group-item-action" href="#" @click.prevent="editMode = !editMode" :class="{ 'active' : editMode }">Edit Budget</a>
-          <a class="list-group-item list-group-item-action" href="/app/new-budget">Delete Budget</a>
+          <router-link
+            exact
+            tag="a"
+            class="list-group-item list-group-item-action"
+            to="/app/new-budget"
+          >New Budget</router-link>
+          <a
+            class="list-group-item list-group-item-action"
+            href="#"
+            @click.prevent="editMode = !editMode"
+            :class="{ 'active' : editMode }"
+          >Edit Budget</a>
+          <a
+            class="list-group-item list-group-item-action"
+            href="#"
+            @click.prevent="removeBudget"
+          >Delete Budget</a>
           <a
             class="list-group-item list-group-item-action bg-dark text-white"
             href="#"
@@ -14,28 +28,28 @@
         </ul>
       </div>
       <div class="col-9">
-        <app-budgetdetail :budget="budget" v-if="!editMode"/>
-        <app-editbudget :budget="budget" v-else/>
+        <app-budgetdetail :budget="budget" v-if="!editMode" />
+        <app-editbudget :budget="budget" v-else />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import BudgetDetail from '../components/BudgetDetail';
-import EditBudget from '../components/EditBudget';
+import BudgetDetail from "../components/BudgetDetail";
+import EditBudget from "../components/EditBudget";
 
-import { EventBus } from '../main';
+import { EventBus } from "../main";
 
 export default {
   components: {
-      'app-budgetdetail': BudgetDetail,
-      'app-editbudget': EditBudget
+    "app-budgetdetail": BudgetDetail,
+    "app-editbudget": EditBudget
   },
   created() {
-    EventBus.$on('refreshBudget', (budget) => {
-        this.budget = budget;
-        this.editMode = false;
+    EventBus.$on("refreshBudget", budget => {
+      this.budget = budget;
+      this.editMode = false;
     });
     const idx = this.$store.getters.budgets
       .map(x => x._id)
@@ -58,6 +72,19 @@ export default {
       editMode: false,
       err: ""
     };
+  },
+  methods: {
+    removeBudget() {
+      if (confirm(`Delete budget?`)) {
+        this.$store
+        .dispatch("removeBudget", {
+        _id: this.$route.params.id,
+        token: this.$store.getters.auth.token
+        })
+        .then(() => this.$router.push('/app/budgets'))
+        .catch(err => (this.err = err));
+      }
+    }
   }
 };
 </script>
